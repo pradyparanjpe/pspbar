1bcbf2ddb9e1f0dad26cba73d3d15c10bc64f892
pradyparanjpe <pradyparanjpe@rediffmail.com>
2020-09-08 12:15:43 +0530

    Better recognise LAN

--- pspbar/shell_dep/netcheck.sh
+++ pspbar/shell_dep/netcheck.sh
@@ -3,7 +3,7 @@
 #
 # Copyright 2020 Pradyumna Paranjape
 #
-## Check for network connectivity at the beginning
+## Check for netknown connectivity at the beginning
 # This file is part of Prady_runcom.
 #
 # Prady_runcom is free software: you can redistribute it and/or modify
@@ -22,10 +22,10 @@
 
 # Variables
 google_ping_cmd="ping 8.8.8.8 -c 1 -q -w 2"
-work_ping_cmd="ping 192.168.1.101 -c 1 4 -q -w 2"
+known_ping_cmd="ping 192.168.1.101 -c 1 4 -q -w 2"
 inter=0
 intra=0
-work=0
+known=0
 home=0
 ip_addr=
 ap_addr=
@@ -50,8 +50,9 @@ export ip_addr
 if [[ -n "$ip_addr" ]]; then
     $google_ping_cmd 2>&1 1>/dev/null && inter=1
     $intra_ping_cmd 2>&1 1>/dev/null && intra=1
-    [[ "${ap_addr}" =~ "192.168.0.1" ]] && home=1
-    [[ "$home" == "0" ]] && $work_ping_cmd 2>&1 1>/dev/null && work=1
+    [[ "$ap_addr" =~ "192.168.0.1" ]] && home=1
+    [[ "$ap_addr" =~ "192.168.1.1" ]] && home=1
+    [[ "$home" == "0" ]] && $known_ping_cmd 2>&1 1>/dev/null && known=1
 fi
 
-echo -e "${ip_addr}\t${ap_addr}\t$(( 8 * $inter + 4 * $intra + 2 * $home + $work ))"
+echo -e "${ip_addr}\t${ap_addr}\t$(( 8 * $inter + 4 * $intra + 2 * $home + $known ))"
